02795c339ef5a389765e80db3b666ee9ba75784d
David A <daayros@protonmail.com>
2023-11-28 15:10:44 +0100

    vercel Pgsql

--- src/lib/vercelPgsql.ts
+++ src/lib/vercelPgsql.ts
@@ -1,5 +1,6 @@
 import { sql } from "@vercel/postgres";
 
+
 export const getAssistantId = async (): Promise<string | undefined> => {
   const { rows } = await sql`SELECT * from admin LIMIT 1`;
   if (rows && rows.length === 1) {

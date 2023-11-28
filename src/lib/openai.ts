ed6cc841f94d6520c1a6cb83a825743831e242e0
David A <daayros@protonmail.com>
2023-11-28 14:27:13 +0100

    to local to cloud storage

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -1,6 +1,6 @@
-// src/lib/openai.ts
 import OpenAI from "openai";
 import fs from "fs";
+import { getAssistantId, updateAssistantId } from "./vercelPgsql";
 const secretKey = process.env.OPENAI_API_KEY;
 
 export const openai = new OpenAI({
@@ -8,13 +8,12 @@ export const openai = new OpenAI({
 });
 
 // Database for storing assistant ID.
-const assistantIdFilePath = "assistantId.txt";
 
 export const createAssistant = async () => {
   // Try to read the assistant ID from the file
   let assistantId;
   try {
-    assistantId = fs.readFileSync(assistantIdFilePath, "utf8");
+    assistantId = await getAssistantId();
   } catch (error) {
     console.log("Assistant ID file not found, creating a new assistant");
   }
@@ -29,7 +28,7 @@ export const createAssistant = async () => {
     });
 
     // Save the assistant ID to a file
-    fs.writeFileSync(assistantIdFilePath, assistantInstance.id);
+    await updateAssistantId(assistantInstance.id);
 
     return assistantInstance;
   } else {
@@ -38,52 +37,58 @@ export const createAssistant = async () => {
   }
 };
 
+export const createThreadAndRun = async (
+  threadId: string | undefined,
+  question: string
+) => {
+  const myAssistant = await createAssistant();
+  const thread = await createThread(threadId);
+  await addMessageToThread(thread.id, question);
+  const run = await createRun(myAssistant.id, thread.id);
+  return { run, thread };
+};
+
 export const createThread = async (threadId: string | undefined) => {
-    try {
-      if (threadId) {
-        return await openai.beta.threads.retrieve(threadId);
-      }
-      return await openai.beta.threads.create();
-    } catch (error: any) {
-      throw error;
+  try {
+    if (threadId) {
+      return await openai.beta.threads.retrieve(threadId);
     }
-  };
+    return await openai.beta.threads.create();
+  } catch (error: any) {
+    throw error;
+  }
+};
 
 export const addMessageToThread = async (
-    threadId: string,
-    question: string
-  ) => {
-    await openai.beta.threads.messages.create(threadId, {
-      role: "user",
-      content: question,
-    });
-  };
-  
+  threadId: string,
+  question: string
+) => {
+  await openai.beta.threads.messages.create(threadId, {
+    role: "user",
+    content: question,
+  });
+};
 
 export const createRun = async (assistantId: string, threadId: string) => {
-    const run = await openai.beta.threads.runs.create(threadId, {
-      assistant_id: assistantId,
-    });
-    return run;
-  };
-  
-  export const statusOfRun = async (threadId: string, runId: string) => {
-    let runStatus = await openai.beta.threads.runs.retrieve(threadId, runId);
-    return runStatus.status;
-  };
+  const run = await openai.beta.threads.runs.create(threadId, {
+    assistant_id: assistantId,
+  });
+  return run;
+};
 
-  export const getMessagesForThread = async (threadId: string) => {
-    const messages = await openai.beta.threads.messages.list(threadId);
-    return messages;
-  };
+export const statusOfRun = async (threadId: string, runId: string) => {
+  let runStatus = await openai.beta.threads.runs.retrieve(threadId, runId);
+  return runStatus.status;
+};
 
-  export const createThreadAndRun = async (
-    threadId: string | undefined,
-    question: string
-  ) => {
-    const myAssistant = await createAssistant();
-    const thread = await createThread(threadId);
-    await addMessageToThread(thread.id, question);
-    const run = await createRun(myAssistant.id, thread.id);
-    return { run, thread };
-  };
+export const getMessagesForThread = async (threadId: string) => {
+  const messages = await openai.beta.threads.messages.list(threadId);
+  return messages;
+};
+
+export const uploadFile = async () => {
+  const file = await openai.files.create({
+    file: fs.createReadStream("mydata.jsonl"),
+    purpose: "fine-tune",
+  });
+};
